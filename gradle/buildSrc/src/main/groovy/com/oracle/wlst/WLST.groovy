3634c61ca49f7e261902f9ec3adfa63d739d743a
Williams, Sion <sion.williams@lv.com>
2014-06-24 16:57:29 +0100

    First version wasn't very 'DRY'. Using a cutom gradle task to make this more extendable.

---
+++ gradle/buildSrc/src/main/groovy/com/oracle/wlst/WLST.groovy
@@ -0,0 +1,46 @@
+package com.oracle.wlst
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Optional
+import org.gradle.api.tasks.TaskAction
+
+/**
+ * Custom Gradle task wrapping the Ant WLST task
+ * @author Sion Williams
+ */
+class WLST extends DefaultTask{
+
+    @Input
+    String fileName
+
+    @Input
+    @Optional
+    String properties
+
+    @Input
+    @Optional
+    String arguments
+
+    @Input
+
+    boolean failOnError = true
+
+    @Input
+    @Optional
+    boolean executeScriptBeforeFile = false
+
+    @Input
+    @Optional
+    boolean debug = false
+
+    @TaskAction
+    void runWlstCommand(){
+        ant.taskdef( name: 'wlst', classname: 'weblogic.ant.taskdefs.management.WLSTTask')
+        ant.wlst( debug: debug,
+                arguments: arguments,
+                failOnError: failOnError,
+                properties: properties,
+                fileName: fileName )
+    }
+}
